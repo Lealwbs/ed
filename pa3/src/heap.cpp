ac98c46b9023e2d2904c93088ee5bc1cf2cbdff6
Lealwbs <italoleallanasantos@gmail.com>
2025-05-08 21:38:14 -0300

    bkp working bases

--- pa3/src/heap.cpp
+++ pa3/src/heap.cpp
@@ -1,3 +1,26 @@
 #include "heap.hpp"
+/*
+public:
+    Heap(int maxsize);
+    ~Heap();
 
+    void Inserir(int x);
+    int Remover();
 
+    //Retorna true caso o heap esteja vazio, false caso contrário.
+    bool Vazio();
+
+private:
+    int GetAncestral(int posicao);
+    int GetSucessorEsq(int posicao);
+    int GetSucessorDir(int posicao);
+
+    int tamanho;
+    int* data;
+
+    /* Funções necessárias para implementar o Heapify recursivo
+        * Você pode apagar elas caso decida implementar o Heapify iterativo
+        
+    void HeapifyPorBaixo(int posicao);
+    void HeapifyPorCima(int posicao); 
+    */
--- pa3/src/main.cpp
+++ pa3/src/main.cpp
@@ -1,7 +1,32 @@
 #include "heap.hpp"
 #include <iostream>
+#include <vector>
 
 int main(){
-    std::cout << "opa" << std::endl;
+    //class Heap MinHeap;
+
+    int tamMinHeap = 0, entrada;
+    std::cin >> tamMinHeap;
+    std::cout << tamMinHeap << std::endl;
+
+    int Vetor[tamMinHeap];
+
+    for(int i=0; i<tamMinHeap; i++){
+
+        std::cin >> entrada;
+        Vetor[i] = entrada;
+        //inserir entrada na estrutura de dados na ordem de leitura
+        //minheapsize ++ 
+
+
+
+        //remover todos os elementos do minheap e imprimilos na ordem de remoção separados por " ", com endl;
+
+
+    };
+
+    for(int i=0; i<tamMinHeap; i++){
+        std::cout <<  Vetor[i]  << std::endl;
+    };
     return 0;
 }

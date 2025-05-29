f796a4dcd8a6cce4d4da3e8a34ed4ddd7a19b800
Lealwbs <italoleallanasantos@gmail.com>
2025-05-28 21:43:02 -0300

    update unionFind.cpp base

--- pa4/src/unionFind.cpp
+++ pa4/src/unionFind.cpp
@@ -1,1 +1,8 @@
 #include "../include/unionFind.hpp"
+
+UnionFind::UnionFind(int quantidade_subconjuntos){};
+UnionFind::~UnionFind(){};
+
+void UnionFind::Make(int x){};
+int UnionFind::Find(int x){};
+void UnionFind::Union(int x, int y){};

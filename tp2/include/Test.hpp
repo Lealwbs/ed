ac86223247ecc7439593f92e4da2c9a9b849bc2c
Lealwbs <italoleallanasantos@gmail.com>
2025-06-02 21:25:47 -0300

    pa2 working base + files

---
+++ tp2/include/Test.hpp
@@ -0,0 +1,14 @@
+#ifndef TEST_HPP
+#define TEST_HPP
+
+#include <stdio.h>
+
+class Test 
+{
+    public:
+        Test();
+        ~Test();
+        void Print();
+};
+
+#endif
---
+++ tp2/src/Main.cpp
@@ -0,0 +1,7 @@
+#include "Test.hpp"
+
+int main(int argc, char* argv[]) {
+    Test* pTest = new Test();
+    pTest->Print();
+    return 0;
+}

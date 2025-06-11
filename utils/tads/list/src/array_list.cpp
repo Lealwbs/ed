5c7a1d63905fc2e6361ae6a8ead58f7540031dde
Leal <italoleallanasantos@gmail.com>
2025-06-11 02:54:03 -0300

    add array_list.cpp implementation

--- utils/tads/list/src/array_list.cpp
+++ utils/tads/list/src/array_list.cpp
@@ -1,1 +1,104 @@
 #include "../include/array_list.hpp"
+#include <iostream>
+
+int ListaArranjo::GetItem(int pos) {
+    if (pos < 0 || pos >= tamanho)
+        return -1;
+
+    return itens[pos];
+};
+
+void ListaArranjo::SetItem(int item, int pos) {
+    if (pos < 0 || pos >= tamanho)
+        return;
+    itens[pos] = item;
+};
+
+void ListaArranjo::InsereInicio(int item) {
+    if (tamanho >= MAXTAM)
+        return;
+
+    for (int i = tamanho; i > 0; i--) {
+        itens[i] = itens[i - 1];
+    };
+
+    itens[0] = item;
+    tamanho++;
+};
+
+void ListaArranjo::InsereFinal(int item) {
+    if (tamanho >= MAXTAM)
+        return;
+
+    itens[tamanho] = item;
+    tamanho++;
+};
+
+void ListaArranjo::InserePosicao(int item, int pos) {
+    if (pos < 0 || pos > tamanho || tamanho >= MAXTAM)
+        return;
+
+    for (int i = tamanho; i > pos; i--) {
+        itens[i] = itens[i - 1];
+    };
+
+    itens[pos] = item;
+    tamanho++;
+};
+
+int ListaArranjo::RemoveInicio() {
+    if (tamanho == 0)
+        return -1;
+
+    int aux = itens[0];
+    for (int i = 0; i < tamanho - 1; i++) {
+        itens[i] = itens[i + 1];
+    };
+    tamanho--;
+    return aux;
+};
+
+int ListaArranjo::RemoveFinal() {
+    if (tamanho == 0)
+        return -1;
+
+    int aux = itens[tamanho - 1];
+    tamanho--;
+    return aux;
+};
+
+int ListaArranjo::RemovePosicao(int pos) {
+    if (pos < 0 || pos >= tamanho || tamanho == 0)
+        return -1;
+
+    int aux = itens[pos];
+
+    for (int i = pos; i < tamanho - 1; i++) {
+        itens[i] = itens[i + 1];
+    };
+
+    tamanho--;
+    return aux;
+};
+
+int ListaArranjo::Pesquisa(int c) {
+    int posicao = -1;
+    for (int i = 0; i < tamanho; i++) {
+        if (itens[i] == c) {
+            posicao = i;
+            break;
+        };
+    };
+    return posicao;
+};
+
+void ListaArranjo::Imprime() {
+    for (int i = 0; i < tamanho; i++) {
+        std::cout << itens[i] << " ";
+    };
+    std::cout << std::endl;
+};
+
+void ListaArranjo::Limpa() {
+    tamanho = 0;
+};
--- utils/tads/list/src/main.cpp
+++ utils/tads/list/src/main.cpp
@@ -4,6 +4,69 @@
 
 int main(){
 
-    std::cout << "Hello World" << std::endl;
+    ListaArranjo vetor = ListaArranjo();
+
+    vetor.InsereInicio(5);
+    vetor.InsereInicio(4);
+    vetor.InsereInicio(3);
+    vetor.InsereInicio(2);
+    vetor.InsereInicio(1);
+
+    vetor.InsereFinal(6);
+    vetor.InsereFinal(7);
+    vetor.InsereFinal(8);
+    vetor.InsereFinal(9);
+    vetor.InsereFinal(0);
+
+    vetor.Imprime();
+
+    vetor.InserePosicao(100, 7);
+
+    vetor.Imprime();
+
+    vetor.SetItem(50, 0);
+
+    vetor.Imprime();
+
+    std::cout << vetor.GetItem(7) << std::endl;
+
+    std::cout << vetor.Pesquisa(100) << std::endl;
+
+    std::cout << "#####" << std::endl << std::endl;
+
+    
+    std::cout << "Tamanho: " << vetor.GetTamanho() << std::endl;
+    vetor.Imprime();
+
+    std::cout << vetor.RemovePosicao(7) << std::endl;
+    std::cout << "Tamanho: " << vetor.GetTamanho() << std::endl;
+    vetor.Imprime();
+
+    std::cout << vetor.RemovePosicao(0) << std::endl;
+    std::cout << "Tamanho: " << vetor.GetTamanho() << std::endl;
+    vetor.Imprime();
+
+    vetor.RemoveFinal();
+    vetor.RemoveFinal();
+    vetor.RemoveInicio();
+    vetor.Imprime();
+
+    vetor.Limpa();
+
+    std::cout << "#####" << std::endl << std::endl;
+
+    for(int i=0; i<100; i++){
+        vetor.InsereFinal(i);
+    };
+
+    vetor.Imprime();
+
+    std::cout << vetor.RemovePosicao(99) << std::endl << std::endl;
+    
+
+    vetor.Imprime();
+
+
+
     return 0;
 }

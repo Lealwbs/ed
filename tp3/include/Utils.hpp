173876a32b5529b6b878f2aab4bf761c0959315a
Leal <italoleallanasantos@gmail.com>
2025-07-03 02:22:24 -0300

    add Utils (eventos e comandos)

---
+++ tp3/include/Utils.hpp
@@ -0,0 +1,43 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+enum TipoComando {
+    EV, // EVENTO
+    PC, // CONSULTA PACOTE
+    CL  // CONSULTA CLIENTE
+};
+
+enum TipoEvento {
+    RG, // Pacote ainda não foi postado (apenas registrado no sistema)
+    AR, // Pacote chegou no armazém e foi armazenado na respectiva seção
+    RM, // Pacote foi removido da seção para tentativa de transporte
+    UR, // Pacote foi rearmazenado por não ser possível transportá-lo
+    TR, // Pacote sendo transportado entre armazéns
+    EN  // Pacote entregue
+};
+
+// ATRIBUTOS DE CADA EVENTO:
+// RG: data_hora EV tipo_evento id_pacote remetente destinatario armazem_origem armazem_destino
+// AR: data_hora EV tipo_evento id_pacote armazem_destino secao_destino
+// RM: data_hora EV tipo_evento id_pacote armazem_destino secao_destino
+// UR: data_hora EV tipo_evento id_pacote armazem_destino secao_destino
+// TR: data_hora EV tipo_evento id_pacote armazem_origem armazem_destino
+// EN: data_hora EV tipo_evento id_pacote armazem_destino
+
+class Utils {
+    public:
+
+        // EVENTOS
+        static TipoEvento stringToTipoEvento(const std::string& str);
+        static std::string tipoEventoToString(TipoEvento tipo);
+        static std::string intToString(int num, int width = 3);
+
+        // COMANDOS
+        static TipoComando stringToTipoComando(const std::string& comando);
+};
+
+#endif
---
+++ tp3/src/Utils.cpp
@@ -0,0 +1,44 @@
+#include "../include/Utils.hpp"
+
+TipoEvento Utils::stringToTipoEvento(const std::string& str) {
+    if (str == "RG") return RG;
+    if (str == "AR") return AR;
+    if (str == "RM") return RM;
+    if (str == "UR") return UR;
+    if (str == "TR") return TR;
+    if (str == "EN") return EN;
+    throw std::invalid_argument("Tipo de evento inválido: " + str);
+};
+
+
+std::string Utils::tipoEventoToString(TipoEvento tipo) {
+    switch (tipo) {
+        case RG: return "RG";
+        case AR: return "AR";
+        case RM: return "RM";
+        case UR: return "UR";
+        case TR: return "TR";
+        case EN: return "EN";
+        default: return "UNKNOWN";
+    };
+};
+
+
+// CONVERTE UM INTEIRO PARA STRING COM PREENCHIMENTO DE ZEROS. EX: 123 ==(Width 7)==> "0000123"
+std::string Utils::intToString(int num, int width) {
+    int strLen = std::to_string(num).length();
+    if (num < 0 || strLen > width)
+        return "ERROR_INT_TO_STRING";
+
+    std::stringstream result;
+    result << std::setw(width) << std::setfill('0') << num;
+    return result.str();
+};
+
+
+TipoComando Utils::stringToTipoComando(const std::string& comando) {
+    if (comando == "EV") return EV; // 0
+    if (comando == "PC") return PC; // 1
+    if (comando == "CL") return CL; // 2
+    throw std::invalid_argument("Comando inválido: " + comando);
+};

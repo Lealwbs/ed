e56f7c6bf3bfdf16e4275bd1a7c260215bef9f3a
Leal <italoleallanasantos@gmail.com>
2025-07-06 20:25:55 -0300

    update the way events are added

--- tp3/src/main.cpp
+++ tp3/src/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 #include "../include/ArvoreALV.hpp"
-#include "../include/VetorEventos.hpp"
+#include "../include/EventosVetor.hpp"
 #include "../include/SistemaLogistico.hpp"
 
 int main(int argc, char** argv) {
@@ -36,30 +36,27 @@ int main(int argc, char** argv) {
             switch(tipoEvento){
                 case TipoEvento::RG:
                     arquivo >> idPacote >> remetente >> destinatario >> armazemOrigem >> armazemDestino;
-                    novoEvento = Evento(tipoEvento, dataHora, idPacote, armazemOrigem, armazemDestino, -1, remetente, destinatario);
-                    sistema.processarEvento(novoEvento);
+                    sistema.processarEvento(Evento(tipoEvento, dataHora, idPacote, armazemOrigem, armazemDestino, -1, remetente, destinatario));
                     break;
 
                 case TipoEvento::TR:
                     arquivo >> idPacote >> armazemOrigem >> armazemDestino;
-                    novoEvento = Evento(tipoEvento, dataHora, idPacote, armazemOrigem, armazemDestino, -1, "", "");
-                    sistema.processarEvento(novoEvento);
+                    sistema.processarEvento(Evento(tipoEvento, dataHora, idPacote, armazemOrigem, armazemDestino, -1, "", ""));
                     break;
 
                 case TipoEvento::EN:
                     arquivo >> idPacote >> armazemDestino; 
-                    novoEvento = Evento(tipoEvento, dataHora, idPacote, -1, armazemDestino, -1, "", "");
-                    sistema.processarEvento(novoEvento);
+                    sistema.processarEvento(Evento(tipoEvento, dataHora, idPacote, -1, armazemDestino, -1, "", ""));
                     break;
 
                 case TipoEvento::AR: case TipoEvento::RM: case TipoEvento::UR: // AR, RM, UR: POSSUEM A MESMA ENTRADA
                     arquivo >> idPacote >> armazemDestino >> secaoDestino;
-                    novoEvento = Evento(tipoEvento, dataHora, idPacote, -1, armazemDestino, secaoDestino, "", "");
-                    sistema.processarEvento(novoEvento);
+                    sistema.processarEvento(Evento(tipoEvento, dataHora, idPacote, -1, armazemDestino, secaoDestino, "", ""));
                     break;
                 
                 default: 
-                    return -1; 
+                    std::cerr << "Tipo de evento inválido: " << strTipoEvento << std::endl;
+                    return 2; 
             };
         };
 
@@ -75,8 +72,10 @@ int main(int argc, char** argv) {
 
     };
 
+    // DEPURAÇÃO
     std::cout << std::endl << "Fim do processamento dos eventos." << std::endl << std::endl;
     sistema.imprimirEventos();
+    // DEPURAÇÃO
 
     arquivo.close();
     return 0;
